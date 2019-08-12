2ef241db44b5ada0e6545700a0cedbef38af20d3
Roman Gorshunov <roman.gorshunov@att.com>
2019-08-12 17:08:04 +0200

    Add tooltip to star symbol which marks core reviewers
    This commit adds a mouseover text to the star symbol which marks
    members of core reviewers team.
    
    Change-Id: I9847729bf36f069c47e841e036859d29262e783c

--- stackalytics/dashboard/static/js/stackalytics-ui.js
+++ stackalytics/dashboard/static/js/stackalytics-ui.js
@@ -139,7 +139,7 @@ function renderTableAndChart(url, container_id, table_id, chart_id, link_param, 
                     }
 
                     if (data[i].core == "master") {
-                        data[i].link += '&nbsp;&#x273B;'
+                        data[i].link += "&nbsp;<span title=\"Core Reviewer\">&#x273B;</span>"
                     } else if (data[i].core) {
                         data[i].link += "&nbsp;&#x272C; <small><i>" + data[i].core + "</i></small>";
                     }
